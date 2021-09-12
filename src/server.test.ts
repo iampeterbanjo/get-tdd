224294846a6597f1f5337c7522afff3422c38ef2
peter.banjo <peter.banjo@samarkand.global>
2021-09-12 07:26:45 +0100

    refactor health test

--- src/server.test.ts
+++ src/server.test.ts
@@ -2,11 +2,17 @@ import fastify from 'fastify';
 import packageJson from '../package.json';
 import { healthRoutes } from './routes/health';
 
+const Server = async () => {
+  const server = fastify();
+  await server.register(healthRoutes);
+
+  return server;
+};
+
 describe(`Given healthRoutes`, () => {
   test(`When GET /health
   Then response is 200`, async () => {
-    const server = fastify();
-    await server.register(healthRoutes);
+    const server = await Server();
 
     const { statusCode } = await server.inject({
       method: 'GET',
@@ -19,8 +25,7 @@ describe(`Given healthRoutes`, () => {
   test(`When GET /health
   And response is 200
   Then payload contains version`, async () => {
-    const server = fastify();
-    await server.register(healthRoutes);
+    const server = await Server();
 
     const { statusCode, payload } = await server.inject({
       method: 'GET',
@@ -30,8 +35,10 @@ describe(`Given healthRoutes`, () => {
     const data = JSON.parse(payload);
 
     expect(statusCode).toEqual(200);
-    expect(data).toEqual(expect.objectContaining({
-      version: packageJson.version
-    }))
+    expect(data).toEqual(
+      expect.objectContaining({
+        version: packageJson.version,
+      })
+    );
   });
 });
