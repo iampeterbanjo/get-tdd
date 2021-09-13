d6be97fb667a0a8628f395a0565bceeac04ab56e
peter.banjo <peter.banjo@samarkand.global>
2021-09-13 07:07:40 +0100

    mount add and multiply routes in api

--- src/api.test.ts
+++ src/api.test.ts
@@ -12,4 +12,28 @@ describe(`Given api`, () => {
 
     expect(statusCode).not.toEqual(404);
   });
+
+  test(`When POST /add
+  Then response is NOT 404`, async () => {
+    const api = await Api();
+
+    const { statusCode } = await api.inject({
+      method: 'POST',
+      url: '/add',
+    });
+
+    expect(statusCode).not.toEqual(404);
+  });
+
+  test(`When POST /multiply
+  Then response is NOT 404`, async () => {
+    const api = await Api();
+
+    const { statusCode } = await api.inject({
+      method: 'POST',
+      url: '/multiply',
+    });
+
+    expect(statusCode).not.toEqual(404);
+  });
 });
--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,14 @@
 import fastify from 'fastify';
+import { addRoutes } from './routes/add';
 import { healthRoutes } from './routes/health';
+import { multiplyRoutes } from './routes/multiply';
 
 export const Api = async (options?: Record<string, any>) => {
   const server = fastify(options);
 
   await server.register(healthRoutes);
+  await server.register(addRoutes);
+  await server.register(multiplyRoutes);
 
   return server;
 };
