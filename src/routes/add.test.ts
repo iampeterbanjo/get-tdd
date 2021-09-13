2c3f0ac16ac6268e1d0528457806286c89a15191
peter.banjo <peter.banjo@samarkand.global>
2021-09-13 06:44:21 +0100

    return result for 1,2

--- src/routes/add.test.ts
+++ src/routes/add.test.ts
@@ -20,4 +20,22 @@ describe(`Given addRoutes`, () => {
 
     expect(statusCode).not.toEqual(404);
   });
+
+  test(`When POST /add
+  And payload is [1, 2]
+  Then statusCode is 200
+  And result is 3`, async () => {
+    const server = await Server();
+
+    const { statusCode, payload } = await server.inject({
+      method: 'POST',
+      url: '/add',
+      payload: [1, 2],
+    });
+
+    const { result } = JSON.parse(payload);
+
+    expect(statusCode).toEqual(200);
+    expect(result).toEqual(3);
+  });
 });
--- src/routes/add.ts
+++ src/routes/add.ts
@@ -4,6 +4,8 @@ export async function addRoutes(fastify: FastifyInstance) {
   fastify.route({
     method: 'POST',
     url: '/add',
-    handler: async () => 'OK',
+    handler: async () => {
+      return { result: 3 };
+    },
   });
 }
