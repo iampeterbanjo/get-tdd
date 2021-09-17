522d7f276b63e046aebf470dce2421af8d001842
iampeterbanjo <gitblame@iampeterbanjo.com>
2021-09-17 08:35:00 +0100

    validate array using json schema

--- src/routes/add.ts
+++ src/routes/add.ts
@@ -4,13 +4,16 @@ export async function addRoutes(fastify: FastifyInstance) {
   fastify.route({
     method: 'POST',
     url: '/add',
+    schema: {
+      body: {
+        type: 'array',
+        items: {
+          type: 'number',
+        },
+      },
+    },
     handler: async (req, reply) => {
       const [first, second] = req.body as number[];
-      if (typeof first !== 'number' || typeof second !== 'number') {
-        return reply.status(400).send({
-          message: 'Numbers only',
-        });
-      }
       const result = first + second;
 
       return { result };
